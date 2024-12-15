e56d32de8f1e43682fd826d2841d3b0174269219
ValmeI <Ignarvalme@gmail.com>
2024-12-15 20:44:42 +0200

    dded support for downloading TV show episodes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import e
 import os
 from threading import TIMEOUT_MAX
 import requests
@@ -16,13 +17,13 @@ def get_video_details(content_id: int):
         )
         mp4_url = "https:" + data["data"]["mainContent"]["medias"][0]["src"]["file"].replace("\\", "")
         return folder_name, title_with_season_episode_year, mp4_url
-    return None, None
+    return None, None, None
 
 
 def download_mp4(heading: str, file_title: str, mp4_url: str):
-    create_folder_name = heading.replace(" ", "_")
-    os.makedirs(create_folder_name, exist_ok=True)
-    file_path = os.path.join(create_folder_name, f"{file_title}.mp4")
+    folder_name = heading.replace(" ", "_")
+    os.makedirs(folder_name, exist_ok=True)
+    file_path = os.path.join(folder_name, f"{file_title}.mp4")
     response = requests.get(mp4_url, stream=True, timeout=TIMEOUT_MAX)
     if response.status_code == 200:
         with open(file_path, "wb") as file:
@@ -30,14 +31,27 @@ def download_mp4(heading: str, file_title: str, mp4_url: str):
                 file.write(chunk)
 
 
-if __name__ == "__main__":
-    # get this video ID from https://err.ee url ID example: https://lasteekraan.err.ee/1609219331
-    video_content_id = 1608776893
-    print(f"Starting download video content id: {video_content_id}")
-    folder_name, file_name, video_url = get_video_details(video_content_id)
+def run_download(video_content_id):
+    name_folder, file_name, video_url = get_video_details(video_content_id)
     print(f'Got video details: "{file_name}" - "{video_url}"')
     if file_name and video_url:
         print(f"Downloading video: {file_name}")
-        download_mp4(folder_name, file_name, video_url)
+        download_mp4(name_folder, file_name, video_url)
     else:
         print("Failed to get video details")
+
+
+if __name__ == "__main__":
+    EPISODES_TO_DOWNLOAD = 18
+    IS_TV_SHOW = True
+    # get this video ID from https://err.ee url ID example: https://lasteekraan.err.ee/1609219331
+    video_content_id = 1608776890
+
+    if IS_TV_SHOW:
+        print(f"Downloading TV show with {EPISODES_TO_DOWNLOAD} episodes")
+        for i in range(EPISODES_TO_DOWNLOAD):
+            run_download(video_content_id)
+            video_content_id += 3
+    else:
+        print("Downloading single video")
+        run_download(video_content_id)
