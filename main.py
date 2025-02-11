a2864725c9332961344ebc65ce8abce5409e38a3
ValmeI <ignarvalme@gmail.com>
2025-02-11 10:02:01 +0200

    progress bar added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import os
 from threading import TIMEOUT_MAX
 import requests
+from tqdm import tqdm
 
 TIMEOUT_MAX = 60
 
@@ -31,9 +32,14 @@ def download_mp4(heading: str, file_title: str, mp4_url: str):
     file_path = os.path.join(folder_name, f"{file_title}.mp4")
     response = requests.get(mp4_url, stream=True, timeout=TIMEOUT_MAX)
     if response.status_code == 200:
+        total = int(response.headers.get("content-length", 0))
         with open(file_path, "wb") as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
+            # Progress bar too see how much is downloaded etc
+            with tqdm(total=total, unit="B", unit_scale=True, desc=file_title) as pbar:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        file.write(chunk)
+                        pbar.update(len(chunk))
 
 
 def run_download(video_content_id: int):
@@ -57,7 +63,7 @@ def extract_video_id(url):
 
 
 if __name__ == "__main__":
-    ERR_URL = "https://lasteekraan.err.ee/1203172/naksitrallid"
+    ERR_URL = "https://lasteekraan.err.ee/1609584401/vaike-allan-inimantenn"
     EPISODES_TO_DOWNLOAD = 18
     IS_TV_SHOW = False
     video_id = extract_video_id(ERR_URL)
